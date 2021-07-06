e387bb9a9c43078d4b1a29dc2b92638a1c4467b1
Imkn1018 <o2oneru2505@gmail.com>
2021-07-06 10:21:04 +0900

    [add] header and footer

---
+++ src/components/organisms/layout/Footer.tsx
@@ -0,0 +1,5 @@
+import React, { memo, VFC } from 'react';
+
+export const Footer: VFC = memo(() => {
+  return <div>Footer</div>;
+});
---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,5 @@
+import React, { memo, VFC } from 'react';
+
+export const Header: VFC = memo(() => {
+  return <div>Header</div>;
+});
