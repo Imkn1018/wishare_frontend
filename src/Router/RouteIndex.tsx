1194fc7b319432bf7cebcb177e3afce65d3170a3
Imkn1018 <o2oneru2505@gmail.com>
2021-07-06 10:40:59 +0900

    [add] .env

--- src/Router/RouteIndex.tsx
+++ src/Router/RouteIndex.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { SignUp } from '../components/pages/Auth/SignUp';
 import { Top } from '../components/pages/Homes/Top';
 
 export const RouteIndex = [
+  // TOP Page
   {
-    path: '/',
+    path: '',
     exact: true,
     children: <Top />,
+  },
+  // 新規登録
+  {
+    path: 'sign_up',
+    exact: false,
+    children: <SignUp />,
   },
 ];
---
+++ src/components/pages/Auth/SignUp.tsx
@@ -0,0 +1,5 @@
+import React, { memo } from 'react';
+
+export const SignUp = memo(() => {
+  return <div>This is Signuppage</div>;
+});
